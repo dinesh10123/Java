0d2d8765994d6c3907c4503892bb9396a79d8d17
dinesh10123 <159063788+dinesh10123@users.noreply.github.com>
2025-09-27 08:02:30 +0530

    Update Main.java

--- DAY9_Exception/Customer_Account/Main.java
+++ DAY9_Exception/Customer_Account/Main.java
@@ -1,1 +1,12 @@
+package customerAccount;
+
+public class Main {
+	public static void main(String []args) {
+		Account acc = new Account("saving",122562,500,-777);
+		
+		Customer c = new Customer("Dinesh", 101, "MJ",860013,acc);
+		c.disp();
+		c.showInterest();
+	}
+}
 
